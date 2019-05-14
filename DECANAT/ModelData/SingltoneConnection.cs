e49a878a73c7eb4d92244e231d1c32e50573f590
Алексей Соколов <alekseyarmany73@yandex.ru>
2019-05-14 10:23:02 +0300

    подключился к ораклу

---
+++ DECANAT/ModelData/SingltoneConnection.cs
@@ -0,0 +1,30 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace DECANAT.Models
+{
+    public class SingltoneConnection
+    {
+        private static OracleConnection connection;
+        private SingltoneConnection() { }
+        public static OracleConnection GetInstance()
+        {
+            if (connection == null)
+            {
+                connection = new OracleConnection();
+                connection.ConnectionString = WebConfigurationManager.ConnectionStrings["OracleConnectionString"].ConnectionString;
+                connection.Open();
+            }
+            return connection;
+        }
+        public static void Disconnect()
+        {
+            connection.Close();
+            connection = null;
+        }
+    }
+}
