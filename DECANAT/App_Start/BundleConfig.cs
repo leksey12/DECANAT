0b6a47ea62b1a479c87faa5c3a0838011af1a4d9
Алексей Соколов <alekseyarmany73@yandex.ru>
2019-05-16 09:01:27 +0400

    ///

--- DECANAT/App_Start/BundleConfig.cs
+++ DECANAT/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace DECANAT
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
